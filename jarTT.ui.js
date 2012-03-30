9e310d47482f78bde9457ba48ec43f7bc015493a
Chris Vickery <chrisinajar@gmail.com>
2012-03-30 17:00:09 -0400

    hyperlinks are the way of the future

--- jarTT.ui.js
+++ jarTT.ui.js
@@ -241,6 +241,6 @@ jarTT.ui = {
 		);
 		box.append("<br /><br /><br /><p>This mod is optimized and based entirely on the suggestions of the <a href=\"http://codingsoundtrack.com\">Coding Soundtrack</a> family.</p>");
 		box.append("<p><span style=\"font-size: 8px;\"><i>(who are a bunch of creepy gingers and should not be trusted).<i></span></p>");
-		box.append("<p><span style=\"font-size: 8px;\"><i>(Shoutout to @jcann for the GM script: http://tinyurl.com/6q8ed8v).<i></span></p>");
+		box.append("<p><span style=\"font-size: 8px;\"><i>(Shoutout to @jcann for the <a href=\"http://tinyurl.com/6q8ed8v\" target=\"_blank\">GM script</a>).<i></span></p>");
 	}
 };
