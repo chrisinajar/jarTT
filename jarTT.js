9c336cb4ebab28b0be89799073fbe4c7576c7a66
Chris Vickery <chrisinajar@gmail.com>
2012-04-01 17:38:52 -0400

    New module loading system, much more flexible, also includes optional devurl for overwriting where it loads jarTT from

--- jarTT.js
+++ jarTT.js
@@ -21,11 +21,46 @@ var oldJarTT = {};
 var jarTTLoad = function(){};
 (function() {
 
+var baseUrl = "https://raw.github.com/chrisinajar/jarTT/master/";
+
+if (localStorage.jarTT_devurl)
+	baseUrl = localStorage.jarTT_devurl;
+
+
 /*********************************
  ****** THIRD PARTY MODULES ******
  *********************************/
 
 var modules = {
+	// main jarTT, most of the code is here
+	'main': {
+		deps: [],
+		url: baseUrl+ 'jarTT.main.js'
+	},
+	// Ability to hook onto events along with the basic event driven stuff like idle timers
+	'events': {
+		deps: ['main'],
+		url: baseUrl+ 'jarTT.events.js'
+	},
+	// Avatar related stuff, basically anything that uses identifyDiv
+	'avatar': {
+		deps: ['events'],
+		url: baseUrl+ 'jarTT.avatar.js'
+	},
+	// UI shit
+	'ui': {
+		deps: ['events', 'storage'],
+		url: baseUrl+ 'jarTT.ui.js'
+	},
+	// HTML5 storage
+	'storage': {
+		deps: ['events'],
+		url: baseUrl+ 'jarTT.storage.js'
+	},
+	'version': {
+		deps: ['ui'],
+		url: baseUrl+ 'jarTT.version.js'
+	}
 	/*
 	myplugin: {
 		deps: ['events', 'storage'],
@@ -34,11 +69,25 @@ var modules = {
 	 */
 };
 
+var autoLoad = [
+	'main',
+	'events',
+	'avatar',
+	'ui',
+	'storage',
+	'version'
+];
+
+if (localStorage.jarTT_modules) {
+	var module_ar = JSON.parse(localStorage.jarTT_modules);
+	autoLoad = autoLoad.concat(module_ar);
+};
+
 /*
  * Load  modules like this:
  * 
 
-jarTTLoad.forceLoad('http://www.awesomemod.com/test/awesomemod.js', 'awesomemod', function(){
+jarTTLoad.loadScript('http://www.awesomemod.com/test/awesomemod.js', 'awesomemod', function(){
 	jarTT.log("That worked!");
 });
 
@@ -57,27 +106,13 @@ jarTT.settings.debug = true;
 
 
 // This file is just a big complicated loader.
+// no longer true --v
 // The majority of this code is creating an object called jarTTLoad which can take either an 
 // object or a string, and a success handler as the second parameter. The object works just like
 // the "comp" variable like 10 lines down from here, and the string will just be a dependencyless string value
 // that is loaded without any additional checking. Don't do that. I should probably make it private.
 
 var wasLoaded = false;
-// These are dependancies. To add a new file, add it here, and list anything that needs to be loaded first (usually just main and events)
-var comp = {
-	// main jarTT, most of the code is here
-	'main': [],
-	// Ability to hook onto events along with the basic event driven stuff like idle timers
-	'events': ['main'],
-	// Avatar related stuff, basically anything that uses identifyDiv
-	'avatar': ['events'],
-	// UI shit
-	'ui': ['events', 'storage'],
-	// HTML5 storage
-	'storage': ['events'],
-	'version': ['ui']
-};
-
 
 // The following code is hideous, but makes the other code cleaner.
 /* First we unload any existing jarTT instances */
@@ -87,74 +122,64 @@ if (typeof jarTT != "undefined" && jarTT != null) {
 	jarTT.unload();
 	jarTT = null;
 }
-var baseUrl = "https://raw.github.com/chrisinajar/jarTT/master/";
-if (oldJarTT.settings && oldJarTT.settings.baseUrl)
-	baseUrl = oldJarTT.settings.baseUrl;
 
-if (localStorage.jarTT_devurl)
-	baseUrl = localStorage.jarTT_devurl;
-
-jarTTLoad = function(arg1,arg2) {
-	if (typeof arg1 == "object")
-		jarTTLoad.dependency(arg1, arg2);
-	else if (typeof arg1 == "string") {
-		jarTTLoad.loadScript(baseUrl + 'jarTT.' + arg1 + '.js', arg1, arg2);
+jarTTLoad = function(mod, cb) {
+	if (mod in modules) {
+		jarTTLoad.dependency(mod, cb);
+	} else {
+		jarTTLoad.loadScript(baseUrl + 'jarTT.' + mod + '.js', mod, cb);
 	}
 };
-jarTTLoad.forceLoad = function(url, name, cb) {
-	jarTTLoad.loadScript(url, name, cb);
-};
-jarTTLoad.ld = [];
-jarTTLoad.cl = [];
 
-jarTTLoad.n = (function(data) {
-	if (++data.i == data.s) {
-		// We're done
-		data.callback();
-		return;
-	}
-	for (tl in data.comp) if ($.inArray(tl, jarTTLoad.ld) == -1) {
-		jarTTLoad.l(tl, data);
+jarTTLoad.dependency = (function(c, h) {
+	if ($.inArray(c, jarTTLoad.ld) != -1)
+		h();
+	var module = modules[c];
+	var toLoad = []
+	for (var i = 0, l = module.deps.length; i<l; ++i) (function(i) {
+		if ($.inArray(module.deps[i], jarTTLoad.ld) == -1)
+			toLoad.push(module.deps[i]);
+	})(i);
+	
+	var count = toLoad.length;
+	var complete = function() {
+		jarTTLoad.l(c, h);
+	};
+	if (count == 0)
+		return complete();
+	var cur = 0;
+	for (var i = 0; i < count; ++i) {
+		jarTTLoad.dependency(toLoad[i], function() {
+			cur++;
+			if (cur == count)
+				complete();
+		});
 	}
 });
 
-jarTTLoad.l = (function (name, data) {
+jarTTLoad.ld = []; // loaded
+jarTTLoad.cl = {}; // currently loading
+
+jarTTLoad.l = (function (name, cb, d) {
 	if (typeof name != "string")
 		return;
 	if (!isNaN(name))
 		return;
 	if ($.inArray(name, jarTTLoad.ld) != -1)
-		return;
-	if ($.inArray(name, jarTTLoad.cl) != -1)
-		return;
-	var canGo = true;
-	for (r in data.comp[name]) {
-		r = data.comp[name][r];
-		if ($.inArray(r, jarTTLoad.ld) == -1) {
-			jarTTLoad.l(r, data);
-			canGo = false;
-		}
-	}
-	if (!canGo) {
+		return cb(d);
+	
+	if (name in jarTTLoad.cl) {
+		var oldcb = jarTTLoad.cl[name];
+		jarTTLoad.cl[name] = function() {oldcb(); cb(d);};
 		return;
 	}
-	jarTTLoad.cl.push(name);
-	jarTTLoad(name, function() {
-		jarTTLoad.n(data);
+	else
+		jarTTLoad.cl[name] = function() {cb(d);};
+	
+	jarTTLoad.loadScript(modules[name].url, name, function() {
+		jarTTLoad.cl[name]();
+		delete jarTTLoad.cl[name];
 	});
-});
-jarTTLoad.dependency = (function(c, h) {
-	var data = {
-		i: -1,
-		s: (function() {
-			var s = 0;
-			for (tl in c) s++;
-			return s;
-		})(),
-		comp: c,
-		callback: h
-	};
-	jarTTLoad.n(data);
 });
 // Code borrowed and altered from LABjs, http://labjs.com/
 // Specifically: https://gist.github.com/603980
@@ -172,39 +197,51 @@ jarTTLoad.loadScript = (function(script, name, h) {
 
 	// Everything after this is stolen from LABjs, I never checked their license but lets just pretend it's definitely something friendly like GPL
     // loading code borrowed directly from LABjs itself
-    setTimeout(function () {
-        if ("item" in head) { // check if ref is still a live node list
-            if (!head[0]) { // append_to node not yet ready
-                setTimeout(arguments.callee, 25);
-                return;
-            }
-            head = head[0]; // reassign from live node list ref to pure node ref -- avoids nasty IE bug where changes to DOM invalidate live node lists
-        }
-        var scriptElem = oDOC.createElement("script"),
-            scriptdone = false;
-        scriptElem.onload = scriptElem.onreadystatechange = function () {
-            if ((scriptElem.readyState && scriptElem.readyState !== "complete" && scriptElem.readyState !== "loaded") || scriptdone) {
-                return false;
-            }
-            scriptElem.onload = scriptElem.onreadystatechange = null;
-            scriptdone = true;
-            handler();
-        };
-        scriptElem.src = script + '?_=' + (new Date()).getTime();
-        head.insertBefore(scriptElem, head.firstChild);
-    }, 0);
+	setTimeout(function () {
+		if ("item" in head) { // check if ref is still a live node list
+			if (!head[0]) { // append_to node not yet ready
+				setTimeout(arguments.callee, 25);
+				return;
+			}
+			head = head[0]; // reassign from live node list ref to pure node ref -- avoids nasty IE bug where changes to DOM invalidate live node lists
+		}
+		var scriptElem = oDOC.createElement("script"),
+			scriptdone = false;
+		scriptElem.onload = scriptElem.onreadystatechange = function () {
+			if ((scriptElem.readyState && scriptElem.readyState !== "complete" && scriptElem.readyState !== "loaded") || scriptdone) {
+				return false;
+			}
+			scriptElem.onload = scriptElem.onreadystatechange = null;
+			scriptdone = true;
+			handler();
+		};
+		scriptElem.src = script + '?_=' + (new Date()).getTime();
+		head.insertBefore(scriptElem, head.firstChild);
+	}, 0);
 
-    // required: shim for FF <= 3.5 not having document.readyState
-    if (oDOC.readyState == null && oDOC.addEventListener) {
-        oDOC.readyState = "loading";
-        oDOC.addEventListener("DOMContentLoaded", handler = function () {
-            oDOC.removeEventListener("DOMContentLoaded", handler, false);
-            oDOC.readyState = "complete";
-        }, false);
-    }
+	// required: shim for FF <= 3.5 not having document.readyState
+	if (oDOC.readyState == null && oDOC.addEventListener) {
+		oDOC.readyState = "loading";
+		oDOC.addEventListener("DOMContentLoaded", handler = function () {
+			oDOC.removeEventListener("DOMContentLoaded", handler, false);
+			oDOC.readyState = "complete";
+		}, false);
+	}
 });
-jarTTLoad(comp, function() {
-	jarTT.events.dispatchEvent("jarTT_loaded");
-});
+
+(function(autoLoad) {
+var count = autoLoad.length;
+var cur = 0;
+
+for (var i = 0; i < count; ++i) {
+	jarTTLoad(autoLoad[i], function() {
+		cur++;
+		if (cur == count)
+			jarTT.events.dispatchEvent("jarTT_loaded");
+	});
+};
+
+})(autoLoad);
+
 })();
 
