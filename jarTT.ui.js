acb3130ea384e2cfff4906984b44fb2aa8297b5b
Jeffrey Cann <cannjeff@gmail.com>
2012-06-13 21:33:10 -0400

    added button for module browser

--- jarTT.ui.js
+++ jarTT.ui.js
@@ -290,6 +290,13 @@ jarTT.ui = {
 				'text': 'Unload jarTT'
 			}).button().click(jarTT.unload)
 		);
+
+		box.append("<br />");
+		box.append("<br />");
+		box.append($("<button />", {
+				'text': 'Module Browser'
+			}).button().click(jarTT.modulebrowser.showModuleBrowser)
+		);
 		box.append("<br /><br /><br /><p>This mod is optimized and based entirely on the suggestions of the <a href=\"http://codingsoundtrack.com\" target=\"_blank\">Coding Soundtrack</a> family.</p>");
 		box.append("<p><span style=\"font-size: 8px;\"><i>(who are a bunch of creepy gingers and should not be trusted).<i></span></p>");
 		box.append("<p><span style=\"font-size: 8px;\"><i>(Thanks to @cannjeff for the heroics and <a href=\"http://tinyurl.com/6q8ed8v\" target=\"_blank\">GM script</a>).<i></span></p>");
