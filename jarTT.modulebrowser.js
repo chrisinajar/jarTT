01b1e8f11420785818ec4a3aa3f69aa0a174f49e
Jeffrey Cann <cannjeff@gmail.com>
2012-06-13 23:34:50 -0400

    testing new module layout

--- jarTT.modulebrowser.js
+++ jarTT.modulebrowser.js
@@ -71,11 +71,11 @@ jarTT.modulebrowser = {
 			}).click(function() {
 				// Show details 
 			}).append($('<div />', { 
-					html: mod,
-					css: { position:'absolute', top:'0px', left:'0px' }
+					html: mod//,
+					//css: { position:'absolute', top:'0px', left:'0px' }
 			})).append($('<input />', { 
-					type: 'checkbox',
-					css: { position:'absolute', bottom:'0px', right:'0px' }
+					type: 'checkbox'//,
+					//css: { position:'absolute', bottom:'0px', right:'0px' }
 			})).appendTo(sideBar);
 		}
 
