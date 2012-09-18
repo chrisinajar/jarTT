a816e489b0c9dd5dc9c2ff22321f2b9c4c2ab22a
Chris Vickery <chris@mitrend.com>
2012-09-18 01:28:24 +0100

    Consolidated version file

--- jarTT.version.js
+++ jarTT.version.js
@@ -79,112 +79,15 @@ jarTT.version = {
 		var hasChanges = false;
 		switch(version) {
 			case 0.0: // first run
-				changes.push("Versioning! Welcome to 1.0!");
-				changes.push("Persistent settings! Set and forget my brothers.");
-				changes.push("Performance optimizations! No more spamming your DOM!");
-				changes.push("MOAR SMIFF!");
+				changes.push("Welcome to the glorious hole that is jarTT. Settings are in the Settings menu!");
+				changes.push("This is where the release notes go! Lists of changes are found here!");
+				changes.push("Better ui compatibility with Turntable Remix (shoutout to overra)");
+				changes.push("Rewrote hide audience support to use their animation system! Added support for opacity based animations, as well.");
 				changes.push("Mildly painful to view release notes!");
 				
-				version = 1.0;
+				//version = 1.3;
+				// let fall through switch set it...
 				showSettings = true;
-				hasChanges = true;
-			case 1.0:
-				changes.push("Put version number into notes");
-
-				version = 1.01;
-				hasChanges = true;
-			case 1.01:
-				changes.push("Updated autobop to be more reliable");
-				changes.push("Fixed dj idle timers appearing behind settings window");
-
-				version = 1.02;
-				hasChanges = true;
-			case 1.02:
-				changes.push("Burp derp herpa derp");
-
-				version = 1.03;
-				hasChanges = true;
-			case 1.03:
-				changes.push("Fixed up some of the content on the Settings page");
-
-				version = 1.05;
-				hasChanges = true;
-			case 1.05:
-				changes.push("New module loading system with developer modes.");
-
-				notes += "I've built a module loading system into jarTT, currently it supports loading based on dependency trees and stores what modules are loaded between sessions. I'm still working on a module browser, " +
-						"but if you're a programmer and want to make a small mod, please get in touch with me and I'll walk you through my system :)";
-				
-				version = 1.06;
-				hasChanges = true;
-			case 1.06:
-				changes.push("Fixed <3's not appearing when in hide audience mode.");
-
-				version = 1.07;
-				hasChanges = true;
-			case 1.07:
-				changes.push("Added toggleable option to kill the awful bubbles with the smite of jar.");
-
-				version = 1.08;
-				hasChanges = true;
-				showSettings = true;
-			case 1.08:
-				changes.push("Laptop stickers no longer go away when you disable the bubbles! :)");
-
-				version = 1.09;
-				hasChanges = true;
-			case 1.09:
-				changes.push("Created Hivemind system for realtime inter-plugin communication! Opt-in!");
-				changes.push("Improved loading/reloading time");
-
-				version = 1.10;
-				hasChanges = true;
-				showSettings = true;
-			case 1.10:
-				changes.push("Fixed the Hivemind loader");
-
-				notes += "Go enable Hivemind. Currently we're just testing the infastructure, but eventually this will change the way you use turntable.fm";
-				version = 1.11;
-				hasChanges = true;
-				showSettings = true;
-			case 1.11:
-				changes.push("Idle times made much much more accurate using hivemind");
-				changes.push("About a billion hivemind bugfixes");
-				
-				version = 1.12;
-				hasChanges = true;
-				showSettings = true;
-			case 1.12:
-				changes.push("Hivemind is now enabled by default");
-				changes.push("Fixes to flow for compatibility with some other TTfm plugins");
-				
-				notes += "Hivemind is now live for everyone, let me know if you have any major problems please! <3 ";
-				version = 1.13;
-				hasChanges = true;
-				showSettings = true;
-			case 1.13: 
-				changes.push("New modules to mute just this song if you click lame multiple times.");
-				changes.push("Better version number schema");
-				
-				notes += "Major.Minor.Bugfix sersion numbering schema. Major number is architecture changes, minor is new features, bug fix is any smaller change.";
-				
-				version = "1.2.0";
-				hasChanges = true;
-				showSettings = true;
-			case "1.2.0":
-				changes.push("Better ui compatibility with Turntable Remix (shoutout to overra)");
-				
-				version = "1.2.1";
-				hasChanges = true;
-			case "1.2.1":
-				changes.push("Changed hide audience to support new canvas based audience.");
-				
-				version = "1.3.0";
-				hasChanges = true;
-			case "1.3.0":
-				changes.push("Rewrote hide audience support to use their animation system! Added support for opacity based animations, as well.");
-
-				version = "1.4.0";
 				hasChanges = true;
 			case "1.4.0":
 				changes.push("Changed hivemind logic to take into account clock scew. Should fix insane values.");
